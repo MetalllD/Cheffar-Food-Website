8ebf068e17d3ff45c3bfe8716559c27fdabf10ea
mitalkamani <45828315+wocmit@users.noreply.github.com>
2020-04-23 23:58:05 +0530

    Responsive Edits
    did some edits to make website responsive

--- main.js
+++ main.js
@@ -79,5 +79,59 @@ window.sr = ScrollReveal();
             viewFactor: 0.4
         })
 
+        function smoothScroll(target,duration){
+ 
+            let tar = document.querySelector(target);
+            let tarpos = tar.getBoundingClientRect().top;
+            let start = window.pageYOffset;
+            let dis = tarpos-start;
+            let startTime = null;
+        
+        
+        function animation(currentTime){
+        
+            if(startTime===null)
+            startTime=currentTime;
+            let timeCollapse = currentTime - startTime;
+            let run = easeInOutQuad(timeCollapse,start,dis,duration);
+            window.scrollTo(0,run);
+        
+            if(timeCollapse<duration){
+                requestAnimationFrame(animation);
+            }
+        }
+        
+        function easeInOutQuad(t, b, c, d) {   //gizma website for easing equations
+            t /= d/2;
+            if (t < 1) return c/2*t*t + b;
+            t--;
+            return -c/2 * (t*(t-2) - 1) + b;
+        }
+        
+            requestAnimationFrame(animation);
+        
+        }
+        
+        
+        let clickMe1=document.querySelector('#clickme1');
+        
+        clickMe1.addEventListener('click',function(){
+            smoothScroll('#menu',1000);
+        })
+
+        let clickMe2=document.querySelector('#clickme2');
+        
+        clickMe2.addEventListener('click',function(){
+            smoothScroll('#aboutus',1500);
+        })
+
+        let clickMe3=document.querySelector('#clickme3');
+        
+        clickMe3.addEventListener('click',function(){
+            smoothScroll('#contact',2000);
+        })
+        
+        
+        
 
 
